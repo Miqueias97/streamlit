c88f8d7ca9449c795d867702d21c4263648f27dd
Miqueias97 <miqueiasnascimentonp@hotmail.com>
2024-05-30 17:00:54 -0300

    api de envios para atualizar streamlit de envios finalizada tempo médio 20 segundos

--- appApi/app.py
+++ appApi/app.py
@@ -1,5 +1,8 @@
 from flask import Flask
-import requests, json, datetime, time, os
+import requests, json, datetime, time, os, asyncio
+from jamef import tokenJamef, apiJamef
+from dotenv import load_dotenv
+load_dotenv()
 
 '''
 def cria_lista():
@@ -24,12 +27,7 @@ def cria_lista():
                 status = ''
                 if i[4] == 'Jadlog':
                     info = jadlog(i[2])
-                    try:
-                        status = info['consulta'][0]['erro']['descricao']
-                    except:
-                        status = info['consulta'][0]['tracking']['eventos']
-                        data = status[len(status) - 1 ]['data']
-                        status = status[len(status) - 1 ]['status']
+                    
 
                 if i[4] == 'Jamef':
                     info = apiJamef(i[2], tkJamef)
@@ -71,11 +69,10 @@ def cria_lista():
 app = Flask(__name__)
 
 
-
 @app.route('/atualiza-envios-chama-nas-mil')
 def index():
-    from jamef import tokenJamef, apiJamef
-    data = []
+    inicio = time.time() 
+    data_inf = []
     for i in requests.get(f'https://script.google.com/macros/s/AKfycbyKPUmiuTnTbGid3to46v_2bCEitpMnzXdPCxaFGq2QEbZmvmoGP8sVzwTJyV4zlSo1mQ/exec').json()['status']:
         if i[0] != '' and i[0] != 'Ticket status':
             payload = {
@@ -87,15 +84,92 @@ def index():
                 'prazo' : str(i[3]),
                 'status_prazo' : i[15],
                 'tk_agend' : i[13],
-                'dt_agend' : str()
+                'dt_agend' : str(i[14]),
+                'cod_post' : str(i[12])
             }
-            data.append(payload)
+            data_inf.append(payload)
         elif i[0] == '':
             break
     
     tk_jamef = tokenJamef()
+    response_apis = []
+    with requests.Session() as session:
+        for i in data_inf:
+            status = 'erro'
+            data = datetime.datetime.today().date()
+            if i['transp'] == 'Jadlog':
+                url = "https://prd-traffic.jadlogtech.com.br/embarcador/api/tracking/consultar"
+                payload = json.dumps({
+                "consulta": [
+                    {
+                    "df": {
+                        "nf": str(i['nf']),
+                        "cnpjRemetente": "17668689000120",
+                        "tpDocumento": 1
+                    }
+                    }
+                ]
+                })
 
-    return json.dumps(tk_jamef)
+                headers = {
+                'Content-Type': 'application/json',
+                'Authorization': f'Bearer {os.getenv("JAD")}'
+                }
+                response = session.post(url, headers=headers, data=payload).json()
+                try:
+                    status = response['consulta'][0]['erro']['descricao']
+                except:
+                    status = response['consulta'][0]['tracking']['eventos']
+                    data = status[len(status) - 1 ]['data']
+                    status = status[len(status) - 1 ]['status']
+
+            elif i['transp'] == 'Correios':
+                url = "https://apps3.correios.com.br/areletronico/v1/ars/ultimoevento"
+                payload = json.dumps({
+                    "objetos": [
+                    str(i['cod_post']),
+                    str(i['cod_post'])
+                    ]
+                })
+                headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': f'Basic {os.getenv("CORREIOS")}=='
+                }
+                response = session.post(url, headers=headers, data=payload).json()
+                try:
+                    status = response[0]['eventos'][0]['descricaoEvento']
+                    data = response[0]['eventos'][0]['dataEvento']
+                except:
+                    status = response[0]['mensagem']
+
+            elif i['transp'] == 'Jamef':
+                url = "https://api.jamef.com.br/rastreamento/ver"
+                payload = json.dumps({
+                    'documentoResponsavelPagamento' : '17668689000120',
+                    'numeroNotaFiscal' : str(i['nf'])
+                })
+                headers = {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + tk_jamef,
+                'Cookie': 'Path=/; Path=/'
+                }
+
+                response = session.post(url, headers=headers, data=payload).json()
+                try:
+                    status = str(response['message']['message']).split(',')[0]
+                except:
+                    status = response['conhecimentos'][0]['historico'][0]['statusRastreamento']
+                    data = response['conhecimentos'][0]['historico'][0]['dataAtualizacao']
+            else:
+                pass
+    
+            info = i
+            info['status'], info['dt_att'] = status, data
+            response_apis.append(info)
+
+    #response_apis = json.dumps(response_apis)
+    return response_apis
+
 
 
 if __name__ == '__main__':
